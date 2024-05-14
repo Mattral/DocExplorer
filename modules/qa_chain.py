5abc9a5c6a2571f0f5951f253c512efd86f44fc0
Min Htet Myet (Mattral) <88831350+Mattral@users.noreply.github.com>
2024-05-14 11:34:39 +0630

    Update qa_chain.py

--- modules/qa_chain.py
+++ modules/qa_chain.py
@@ -1,48 +1,29 @@
-# from langchain.retrievers.self_query.base import SelfQueryRetriever
-# from langchain.chains.query_constructor.base import AttributeInfo
-
 from langchain.chains import RetrievalQA
+from langchain.retrievers import QdrantRetriever
 
 def create_chain(local_llm, vectordb):
+    retriever = QdrantRetriever(
+        client=vectordb,
+        collection_name="my_facts",
+        embedding_function=encoder.encode,
+        distance_func='cosine'
+    )
 
-    # memory = ConversationBufferMemory(memory_key="chat_history", return_messages=True)
-
-    # metadata_field_info = [
-    # AttributeInfo(
-    #     name="source",
-    #     description="The neme of pdf",
-    #     type="string or list[string]",
-    # ),
-    # AttributeInfo(
-    #     name="page",
-    #     description="The page no of the pdf",
-    #     type="integer",
-    # ),
-    # ]
-
-    # document_content_description = "Information about the PDF's page and name of pdf"
-    # retriever = SelfQueryRetriever.from_llm(
-    #     local_llm, vectordb, document_content_description, metadata_field_info, verbose=False,
-    #     search_kwargs={"fetch_k": 3}
-    # )
-
-
-    retriever = vectordb.as_retriever(search_type="mmr", search_kwargs={'fetch_k': 3})
-
-    qa = RetrievalQA.from_chain_type(llm=local_llm,
-                                     chain_type="stuff",
-                                     retriever=retriever,
-                                     return_source_documents=True,)
-
+    qa = RetrievalQA.from_chain_type(
+        llm=local_llm,
+        chain_type="stuff",
+        retriever=retriever,
+        return_source_documents=True,
+    )
 
     qa.combine_documents_chain.llm_chain.prompt.template = '''
     Your name is Ash.
-    Use the following pieces of context to answer the users question and that
+    Use the following pieces of context to answer the user's question and that
     information consist of provided pdf details by user.
-    If you don't know the answer, just apologies and say that you couldn't
-    find relevent content to answer this question, don't try to
+    If you don't know the answer, just apologize and say that you couldn't
+    find relevant content to answer this question, don't try to
     make anything from yourself just use the provided context. And always answer
-    in conversationl freindly way. And don't make any Follow-up Question and don't
+    in a conversational friendly way. And don't make any Follow-up Question and don't
     provide any further information just answer the question.
     Always answer from the perspective of being Ash.
     ----------------
